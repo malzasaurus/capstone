c693e0869722357c8e24f52f16aa0481ca85069b
Mallory Payne <mallory.payne18@gmail.com>
2016-06-28 11:03:26 -0400

    added some formatting to the bug list

--- browser/js/applications/applications.js
+++ browser/js/applications/applications.js
@@ -1,7 +1,7 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('indvApp', {
         url: '/applications/:appID',
-        templateUrl: 'js/applications/applications.html',
+        templateUrl: 'js/applications/indvApp.html',
         controller: 'AppCtrl', 
         resolve: {
         	allBugs: function(AppFactory, $stateParams){
