3698a0c91354050f17d2bdd36b4d2c1657aa0e03
ohhheyyyy <ashley.hartt@gmail.com>
2016-06-29 13:04:27 -0400

    Successfully build URL for each unique app

--- browser/js/about/about.js
+++ browser/js/about/about.js
@@ -1,4 +1,4 @@
-app.config(function ($stateProvider) {
+app.config(function($stateProvider) {
 
     // Register our *about* state.
     $stateProvider.state('about', {
@@ -9,9 +9,79 @@ app.config(function ($stateProvider) {
 
 });
 
-app.controller('AboutController', function ($scope, FullstackPics) {
-
+app.controller('AboutController', function($scope, $log, $http, $routeParams, $location, FullstackPics) {
+ 
+   // $log.warn("$location.path split on slashes is: ", $location.path().split("/").unshift());
     // Images of beautiful Fullstack people.
     $scope.images = _.shuffle(FullstackPics);
 
+    // var dataSource = 'https://real-time-web-dashboard-bsullins.c9.io/modules/5 - Working with Data/getTrendData.php?format=json';
+    $scope.appId = $location.path().split("/").unshift();
+
+    $scope.dataSource = 'http://localhost:1337/api/applications/' + $scope.appId + '/bugs';
+
+    $http.get('http://localhost:1337/api/applications/' + appId)
+    .success(function(data) {
+        $scope.appId = $routeParams.appId;
+    });
+
+    function requestData() {
+        $.ajax({
+            url: dataSource,
+            success: function(points) {
+
+                chart.series[0].setData(
+                    points,
+                    true
+                );
+
+                // refresh after X miliseconds
+                setTimeout(requestData, 1000);
+            },
+            cache: false
+        });
+    }
+
+    // Testing HighCharts Data
+    //     $scope.chartOptions = {
+
+    //     chart: {
+    //         renderTo: 'chart-container',
+    //         type: 'bar'
+    //     },
+    //     title: {
+    //         text: 'Sales by Sector'
+    //     },
+    //     xAxis: {
+    //         categories: ['Tech', 'Retail', 'Services', 'Other']
+    //     },
+    //     yAxis: {
+    //         title: {
+    //             text: 'Sales in Thousands'
+    //         }
+    //     },
+    //     series: [{
+    //         name: 'Sales Overall',
+    //         data: [100, 33, 58, 77]
+    //     }]
+    // };
+
+
+
+});
+
+app.directive('chartData', function() {
+    return {
+        restrict: 'E',
+        template: '<div class="chart-container"></div>',
+        scope: {
+            options: '='
+        },
+        link: function(scope, element) {
+            //console.log("element looks like: ", element, "\n", "scope.options looks like: ", scope.options);
+            Highcharts.chart(element[0], scope.options);
+        },
+        controller: 'AboutController'
+
+    };
 });
