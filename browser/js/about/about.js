cdc756c9d370fa89d121c99bbf5d1e9e706c5ebf
ohhheyyyy <ashley.hartt@gmail.com>
2016-06-29 18:33:06 -0400

    Render charts with our API data, create factory to grab API data

--- browser/js/about/about.js
+++ browser/js/about/about.js
@@ -9,79 +9,87 @@ app.config(function($stateProvider) {
 
 });
 
-app.controller('AboutController', function($scope, $log, $http, $routeParams, $location, FullstackPics) {
- 
-   // $log.warn("$location.path split on slashes is: ", $location.path().split("/").unshift());
+app.controller('AboutController', function($scope, $log, $http, $routeParams, $location, FullstackPics, AboutFactory) {
+
     // Images of beautiful Fullstack people.
     $scope.images = _.shuffle(FullstackPics);
 
-    // var dataSource = 'https://real-time-web-dashboard-bsullins.c9.io/modules/5 - Working with Data/getTrendData.php?format=json';
-    $scope.appId = $location.path().split("/").unshift();
-
+    // $scope.appId = $location.path().split("/").unshift();
+    $scope.appId = 2;
     $scope.dataSource = 'http://localhost:1337/api/applications/' + $scope.appId + '/bugs';
 
-    $http.get('http://localhost:1337/api/applications/' + appId)
-    .success(function(data) {
-        $scope.appId = $routeParams.appId;
-    });
 
-    function requestData() {
-        $.ajax({
-            url: dataSource,
-            success: function(points) {
+    AboutFactory.requestData()
+        .then(function(bugsData) {
+    
+            $('#line-chart').highcharts({
+                  title: {
+                    text: 'Line Chart'
+                },
+                xAxis: {
+                    categories: []
+                },
+                series: [{
+                    data: bugsData.map(function(obj){
+                        console.log("obj.id looks like: ", obj.id);
+                        return obj.id;
+                    })                        
+                }]
+            });
 
-                chart.series[0].setData(
-                    points,
-                    true
-                );
+              $('#bar-chart').highcharts({
+                  title: {
+                    text: 'Bar Chart'
+                },
+                xAxis: {
+                    categories: ['Apples', 'Bananas', 'Oranges']
+                },
+                series: [{
+                    type: 'bar',
+                    data: bugsData.map(function(obj){
+                        console.log("obj.id looks like: ", obj.id);
+                        return obj.id;
+                    })
+                    //[1, 2, 3]
+                        
+                }]
+            });
 
-                // refresh after X miliseconds
-                setTimeout(requestData, 1000);
-            },
-            cache: false
+                 $('#pie-chart').highcharts({
+                  title: {
+                    text: 'Pie Chart'
+                },
+                xAxis: {
+                    categories: ['Apples', 'Bananas', 'Oranges']
+                },
+                series: [{
+                    type: 'pie',
+                    data: bugsData.map(function(obj){
+                        console.log("obj.id looks like: ", obj.id);
+                        return obj.id;
+                    })                        
+                }]
+            });
+           
+        })
+        .catch(function(err) {
+            console.error(err);
         });
-    }
-
-    // Testing HighCharts Data
-    //     $scope.chartOptions = {
-
-    //     chart: {
-    //         renderTo: 'chart-container',
-    //         type: 'bar'
-    //     },
-    //     title: {
-    //         text: 'Sales by Sector'
-    //     },
-    //     xAxis: {
-    //         categories: ['Tech', 'Retail', 'Services', 'Other']
-    //     },
-    //     yAxis: {
-    //         title: {
-    //             text: 'Sales in Thousands'
-    //         }
-    //     },
-    //     series: [{
-    //         name: 'Sales Overall',
-    //         data: [100, 33, 58, 77]
-    //     }]
-    // };
-
-
 
 });
 
-app.directive('chartData', function() {
-    return {
-        restrict: 'E',
-        template: '<div class="chart-container"></div>',
-        scope: {
-            options: '='
-        },
-        link: function(scope, element) {
-            //console.log("element looks like: ", element, "\n", "scope.options looks like: ", scope.options);
-            Highcharts.chart(element[0], scope.options);
-        },
-        controller: 'AboutController'
-
-    };
-});
+// app.directive('chartData', function() {
+//     return {
+//         restrict: 'E',
+//         template: '<div></div>',
+//         scope: {
+//             options: '='
+//         },
+//         link: function(scope, element) {
+//             // console.log("chartOptions is: ", scope.chartOptions);
+//             console.log("element[0] looks like: ", element[0]);
+//             console.log("scope.options looks like: ", scope.options);
+//             Highcharts.chart(element[0], scope.options);
+//         }
+//     };
+// });
