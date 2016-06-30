b6a67db182d558c37a93abd2b14a4d53d8e88064
ohhheyyyy <ashley.hartt@gmail.com>
2016-06-30 12:52:38 -0400

    Move charts functions, controller, and factory to applications directory; preserve that functionality in about

--- browser/js/about/about.js
+++ browser/js/about/about.js
@@ -1,3 +1,21 @@
+// app.config(function ($stateProvider) {
+
+//     // Register our *about* state.
+//     $stateProvider.state('about', {
+//         url: '/about',
+//         controller: 'AboutController',
+//         templateUrl: 'js/about/about.html'
+//     });
+
+// });
+
+// app.controller('AboutController', function ($scope, FullstackPics) {
+
+//     // Images of beautiful Fullstack people.
+//     $scope.images = _.shuffle(FullstackPics);
+
+// });
+
 app.config(function($stateProvider) {
 
     // Register our *about* state.
@@ -9,7 +27,7 @@ app.config(function($stateProvider) {
 
 });
 
-app.controller('AboutController', function($scope, $log, $http, $routeParams, $location, FullstackPics, AboutFactory) {
+app.controller('AboutController', function($scope, $log, FullstackPics, AboutFactory) {
 
     AboutFactory.requestData()
         .then(function(bugsData) {
@@ -23,9 +41,7 @@ app.controller('AboutController', function($scope, $log, $http, $routeParams, $l
                         assignmentName[bugsData[i].assignment]++;
                     }
                 }
-                //return assignmentName;
 
-                
                 var arr = [];
                 for (var key in assignmentName) {
                     if (assignmentName.hasOwnProperty(key)) {
@@ -33,9 +49,7 @@ app.controller('AboutController', function($scope, $log, $http, $routeParams, $l
                         arr.push(tempArray);
                     }
                 }
-                //console.log("arrrrrrrr you gonna work? ", arr);
                 return arr;
-                
             }
 
             $log.warn("getPersonWorking is: ", getPersonWorking(bugsData));
@@ -54,8 +68,6 @@ app.controller('AboutController', function($scope, $log, $http, $routeParams, $l
             $log.warn("assignmentCount is: ", assignmentCount(bugsData));
             $log.warn("assignmentCount type: ", typeof assignmentCount(bugsData)[1]);
 
-
-
             $('#line-chart').highcharts({
                 title: {
                     text: 'Line Chart'
@@ -65,7 +77,6 @@ app.controller('AboutController', function($scope, $log, $http, $routeParams, $l
                 },
                 series: [{
                     data: bugsData.map(function(obj) {
-                        // console.log("obj.id looks like: ", obj.id);
                         return obj.id;
                     })
                 }]
@@ -81,11 +92,8 @@ app.controller('AboutController', function($scope, $log, $http, $routeParams, $l
                 series: [{
                     type: 'bar',
                     data: bugsData.map(function(obj) {
-                        // console.log("obj.id looks like: ", obj.id);
                         return obj.id;
                     })
-
-
                 }]
             });
 
@@ -115,49 +123,10 @@ app.controller('AboutController', function($scope, $log, $http, $routeParams, $l
                 series: [{
                     type: 'pie',
                     data: getPersonWorking(bugsData)
-                    //[
-
-                        // ['Firefox', 45],
-                        // ['IE', 26.8],
-                        // ['Safari', 8.5],
-                        // ['Opera', 6.2],
-                        // ['Others', 0.7]
-                    //],
-                    //data: assignmentCount(bugsData)
-                    // data: [
-                    //     {
-                    //         name: weGotTheNames(),
-                    //         y: weGotTheNamesValues()
-                    //     }, {
-                    //         name: "Danielle", 
-                    //         y: 4                          
-                    //     },
-                    //     { 
-                    //         name: "Ashley", 
-                    //         y: 2 
-                    //     }
-                    // ]
                 }]
             });
         })
         .catch(function(err) {
             console.error(err);
         });
-
-
 });
-// app.directive('chartData', function() {
-//     return {
-//         restrict: 'E',
-//         template: '<div></div>',
-//         scope: {
-//             options: '='
-//         },
-//         link: function(scope, element) {
-//             // console.log("chartOptions is: ", scope.chartOptions);
-//             console.log("element[0] looks like: ", element[0]);
-//             console.log("scope.options looks like: ", scope.options);
-//             Highcharts.chart(element[0], scope.options);
-//         }
-//     };
-// });
