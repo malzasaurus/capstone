94aebae9333cc7560c7657d0185480074cb8251b
ohhheyyyy <ashley.hartt@gmail.com>
2016-07-07 16:28:05 -0400

    Change _db.js

--- server/db/_db.js
+++ server/db/_db.js
@@ -1,7 +1,21 @@
 var path = require('path');
 var Sequelize = require('sequelize');
-
+var db;
 var env = require(path.join(__dirname, '../env'));
-var db = new Sequelize(env.DATABASE_URI);
+//var db = new Sequelize(env.DATABASE_URI);
+
+if(process.env.DATABASE_URL){
+ db = new Sequelize(process.env.DATABASE_URL, {
+   dialect: 'postgres',
+   port: 5432,
+   logging: false
+ });
+} else {
+ db = new Sequelize('grasshopperdb', '', null, {
+   dialect: 'postgres',
+   port: 5432,
+   logging: false
+ });
+}
 
 module.exports = db;
