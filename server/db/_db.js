6bb3fce75594ef4973d56bfa09980249aa45fa68
ohhheyyyy <ashley.hartt@gmail.com>
2016-07-07 18:03:54 -0400

    Change heroku settings

--- server/db/_db.js
+++ server/db/_db.js
@@ -1,21 +1,8 @@
 var path = require('path');
 var Sequelize = require('sequelize');
-var db;
-var env = require(path.join(__dirname, '../env'));
-//var db = new Sequelize(env.DATABASE_URI);
 
-if(process.env.DATABASE_URL){
- db = new Sequelize(process.env.DATABASE_URL, {
-   dialect: 'postgres',
-   port: 5432,
-   logging: false
- });
-} else {
- db = new Sequelize('grasshopperdb', '', null, {
-   dialect: 'postgres',
-   port: 5432,
-   logging: false
- });
-}
+var env = require(path.join(__dirname, '../env'));
+console.log("look at my ENV: ", env);
+var db = new Sequelize(env.DATABASE_URI);
 
 module.exports = db;
