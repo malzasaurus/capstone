7ee34f9f33de234e7ce4bc9f2a01d93e854f0fd7
Nichole Bates <nbates88@gmail.com>
2016-07-08 15:09:55 -0400

    fixed create new user with github issue

--- server/env/development.js
+++ server/env/development.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   "GITHUB": {
     "clientID": "73ae3d75962b493faf22",
-    "clientSecret": "INSERT_GITHUB_CLIENT_SECRET_HERE",
+    "clientSecret": "xxxxxxxxxxxx",
     "callbackURL": "http://127.0.0.1:1337/auth/github/callback"
   }
 };
