a2f999d0f069f9c19db5c125a61c5ceebd0f2702
ohhheyyyy <ashley.hartt@gmail.com>
2016-07-07 20:31:53 -0400

    Try to address autoscroll problem when changing view

--- browser/js/guide/guide.js
+++ browser/js/guide/guide.js
@@ -4,3 +4,15 @@ app.config(function ($stateProvider) {
         templateUrl: 'js/guide/guide.html'
     });
 });
+
+app.controller('ScrollController', ['$scope', '$location', '$anchorScroll',
+  function ($scope, $location, $anchorScroll) {
+    $scope.gotoBottom = function() {
+      // set the location.hash to the id of
+      // the element you wish to scroll to.
+      $location.hash('top');
+
+      // call $anchorScroll()
+      $anchorScroll();
+    };
+  }]);
